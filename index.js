39338246436944241302728cd78fe2a665d7b459
Abhay VIshnoi <abhayvishnoi6@gmail.com>
2024-10-06 20:03:13 +0000

    added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const dbMap={}
+counter=0
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -17,6 +18,23 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/shorturl/:shortUrl', function(req, res) {
+  res.redirect(dbMap[counter])
+});
+
+app.post('/api/shorturl', function(req, res) {
+  // URL regex pattern
+  const {url}=req.body
+const urlRegex = /^(https?:\/\/)?(www\.)?([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}(\/[^\s]*)?$/;
+
+if (!urlRegex.test(url)) {
+  return res.json({ error: 'invalid url' });
+}
+  dbMap[counter]=url
+  res.json({ original_url : url, short_url : counter});
+  counter+=1
 });
 
 app.listen(port, function() {
